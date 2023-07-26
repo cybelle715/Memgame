6f4d8bc8a536ba2cf1b8749a3d799a0cf0809c1e
cybelle715 <dadjonick@gmail.com>
2023-07-26 15:16:16 +0100

    first commit

---
+++ src/images/flags.js
@@ -0,0 +1,69 @@
+import car1 from "./01.jpg";
+import car2 from "./02.jpg";
+import car3 from "./03.jpg";
+import car4 from "./04.jpg";
+import car5 from "./05.jpg";
+import car6 from "./06.jpg";
+import car7 from "./07.jpg";
+import car8 from "./08.jpg";
+import car9 from "./09.jpg";
+import car10 from "./10.jpg";
+import car11 from "./11.jpg";
+import car12 from "./12.jpg";
+
+const flags = [
+    {
+        id: 0,
+        flag: car1,
+    },
+    {
+        id: 1,
+        flag: car2,
+    },
+    {
+        id: 2,
+        flag: car3,
+    },
+    {
+        id: 3,
+        flag: car4,
+    },
+    {
+        id: 4,
+        flag: car5,
+    },
+    {
+        id: 5,
+        flag: car6,
+    },
+    {
+        id: 6,
+        flag: car7,
+    },
+    {
+        id: 7,
+        flag: car8,
+    },
+    {
+        id: 8,
+        flag: car9,
+    },
+    {
+        id: 9,
+        flag: car10,
+    },
+    {
+        id: 10,
+        flag: car11,
+    },
+    {
+        id: 11,
+        flag: car12,
+    },
+];
+
+const flagsArray = () => {
+    return flags;
+};
+
+export default flagsArray();
